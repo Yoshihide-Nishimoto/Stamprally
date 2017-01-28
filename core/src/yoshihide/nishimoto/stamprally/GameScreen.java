78e3d74f405c09a8bc34f79b3a726a30e6ea772e
Nishimoto <nishimoto20100403@gmail.com>
2017-01-28 21:33:36 +0900

    GameScreen修正

--- core/src/yoshihide/nishimoto/stamprally/GameScreen.java
+++ core/src/yoshihide/nishimoto/stamprally/GameScreen.java
@@ -8,18 +8,23 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.input.GestureDetector;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 
-public class GameScreen extends ScreenAdapter {
+public class GameScreen extends ScreenAdapter implements GestureDetector.GestureListener {
 
     static final float CAMERA_WIDTH = 10;
     static final float CAMERA_HEIGHT = 15;
 
+    private float mBg_width,mBg_height;
+
     private double latitude,longitude;
     private String message,provider;
     private boolean networkenabled;
+    private float testZoom,testIni;
 
     private Stamprally mGame;
 
@@ -32,6 +37,8 @@ public class GameScreen extends ScreenAdapter {
     BitmapFont mFont;
 
     Player mPlayer;
+
+    GestureDetector gestureDetector;
 
     public GameScreen(Stamprally game) {
 
@@ -59,13 +66,15 @@ public class GameScreen extends ScreenAdapter {
 
         Texture playerTexture = new Texture("player.png");
         mPlayer = new Player(playerTexture, 0, 0, 300, 300);
-        //mPlayer.setPosition(CAMERA_WIDTH / 2 - mPlayer.getWidth() , CAMERA_HEIGHT / 2 - mPlayer.getHeight());
         mPlayer.setPosition(CAMERA_WIDTH / 2 - mPlayer.getWidth() / 2 , CAMERA_HEIGHT / 2 - mPlayer.getHeight() / 2);
 
         mFont = new BitmapFont(Gdx.files.internal("font.fnt"), Gdx.files.internal("font.png"), false);
         mFont.getData().setScale(0.05f);
 
         mTouchPoint = new Vector3();
+
+        gestureDetector = new GestureDetector(this);
+        Gdx.input.setInputProcessor(gestureDetector);
 
     }
 
@@ -106,17 +115,21 @@ public class GameScreen extends ScreenAdapter {
             mGuiViewPort.unproject(mTouchPoint.set(Gdx.input.getX(), Gdx.input.getY(), 0));
         }
 
-        mFont.draw(mGame.batch, "X: " + mTouchPoint.x,1, CAMERA_HEIGHT - 1);
-        mFont.draw(mGame.batch, "Y: " + mTouchPoint.y,1, CAMERA_HEIGHT - 2);
-        mFont.draw(mGame.batch, "経度: " + this.latitude,1, CAMERA_HEIGHT - 3);
-        mFont.draw(mGame.batch, "緯度: " + this.longitude,1, CAMERA_HEIGHT - 4);
-        mFont.draw(mGame.batch, "メッセージ: " + this.message,1, CAMERA_HEIGHT - 5);
-        mFont.draw(mGame.batch, "プロバイダー: " + "1.1",1, CAMERA_HEIGHT - 6);
-        mFont.draw(mGame.batch, "ネットワーク: " + this.networkenabled,1, CAMERA_HEIGHT - 7);
-        mFont.draw(mGame.batch, "補正: " + (mCamera.position.x - CAMERA_WIDTH / 2 - cor_latitude * CAMERA_WIDTH / (float) 0.000220 * 4),1, CAMERA_HEIGHT - 8);
-        mFont.draw(mGame.batch, "カメラ: " + mCamera.position.x,1, CAMERA_HEIGHT - 9);
+        mFont.draw(mGame.batch, "x" + mTouchPoint.x,1, CAMERA_HEIGHT - 1);
+        mFont.draw(mGame.batch, "y" + mTouchPoint.y,1, CAMERA_HEIGHT - 2);
+        mFont.draw(mGame.batch, "latitude" + this.latitude,1, CAMERA_HEIGHT - 3);
+        mFont.draw(mGame.batch, "longitude" + this.longitude,1, CAMERA_HEIGHT - 4);
+        mFont.draw(mGame.batch, "message" + this.message,1, CAMERA_HEIGHT - 5);
+        mFont.draw(mGame.batch, "provider" + "1.1",1, CAMERA_HEIGHT - 6);
+        mFont.draw(mGame.batch, "network" + this.networkenabled,1, CAMERA_HEIGHT - 7);
+        mFont.draw(mGame.batch, "correction" + (mCamera.position.x - CAMERA_WIDTH / 2 - cor_latitude * CAMERA_WIDTH / (float) 0.000220 * 4),1, CAMERA_HEIGHT - 8);
+        mFont.draw(mGame.batch, "camera" + mCamera.position.x,1, CAMERA_HEIGHT - 9);
+        mFont.draw(mGame.batch, "dis" + testZoom,1, CAMERA_HEIGHT - 10);
+        mFont.draw(mGame.batch, "ini" + testIni,1, CAMERA_HEIGHT - 11);
         mGame.batch.end();
     }
+
+
 
     @Override
     public void resize(int width, int height) {
@@ -144,4 +157,75 @@ public class GameScreen extends ScreenAdapter {
         this.networkenabled = networkenabled;
     }
 
+    @Override
+    public boolean touchDown(float x, float y, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean tap(float x, float y, int count, int button) {
+        mBg_width = CAMERA_WIDTH * (float)0.6;
+        mBg_height = CAMERA_HEIGHT * (float)0.6;
+        mCamera.setToOrtho(false, mBg_width, mBg_height);
+        mPlayer.setPosition(mBg_width / 2 - mPlayer.getWidth() / 2 , mBg_height / 2 - mPlayer.getHeight() / 2);
+        testZoom = 1;
+        return true;
+    }
+
+    @Override
+    public boolean longPress(float x, float y) {
+        return false;
+    }
+
+    @Override
+    public boolean fling(float velocityX, float velocityY, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean pan(float x, float y, float deltaX, float deltaY) {
+        return false;
+    }
+
+    @Override
+    public boolean panStop(float x, float y, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean zoom(float initialDistance, float distance) {
+//        mBg.setSize(distance,distance);
+        testZoom = distance;
+        testIni = initialDistance;
+        mBg_width = mBg_width * distance / initialDistance;
+        mBg_height = mBg_height * distance / initialDistance;
+
+
+        if (mBg_width > CAMERA_WIDTH) {
+
+            mBg_width = CAMERA_WIDTH;
+            mBg_height = CAMERA_HEIGHT;
+
+        } else if (mBg_width < CAMERA_WIDTH * 0.8) {
+
+            mBg_width = CAMERA_WIDTH * (float)0.8;
+            mBg_height = CAMERA_HEIGHT * (float)0.8;
+
+        }
+
+        mCamera.setToOrtho(false, mBg_width, mBg_height);
+        mPlayer.setPosition(CAMERA_WIDTH / 2 - mPlayer.getWidth() / 2 , CAMERA_HEIGHT / 2 - mPlayer.getHeight() / 2);
+
+        return true;
+    }
+
+    @Override
+    public boolean pinch(Vector2 initialPointer1, Vector2 initialPointer2, Vector2 pointer1, Vector2 pointer2) {
+        return false;
+    }
+
+    @Override
+    public void pinchStop() {
+
+    }
 }
